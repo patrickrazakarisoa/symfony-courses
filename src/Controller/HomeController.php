635c1ac33ee0272aeb068a341c2d55a2fbd1ec31
kenjy <kenjyzakary@hotmail.com>
2021-01-21 16:12:36 +0100

    exercice partie 2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,6 +14,41 @@ class HomeController extends AbstractController
      */
     public function home()
     {
-        return $this->render('base.html.twig');
+        $contacts = [
+            [
+                'id' => 1,
+                'nom' => 'Durant',
+                'prenom' => 'Maire',
+                'telephone' => "0786495705"
+            ],
+            [
+                'id' => 1,
+                'nom' => 'Dubois',
+                'prenom' => 'Jean',
+                'telephone' => "0258484957"
+            ],
+            [
+                'id' => 1,
+                'nom' => 'Smith',
+                'prenom' => 'Mike',
+                'telephone' => "0781574865"
+            ],
+            [
+                'id' => 1,
+                'nom' => 'Lamar',
+                'prenom' => 'Bob',
+                'telephone' => "07654481547"
+            ],
+        ];
+
+        return $this->render('home.html.twig', ["contacts" => $contacts]);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('contact.html.twig');
     }
 }
